5dd182f0ff8823fb814ba96f655e4ac4a0f35944
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-10-19 11:13:44 +0200

    server hello world

--- back/server.js
+++ back/server.js
@@ -1,2 +1,19 @@
 console.log("starting server");
 
+const express = require("express");
+const serveIndex = require("serve-index");
+
+const app = express();
+
+app.get("/titi", (req, res, next) => {
+  console.log("req.url", req.url);
+  next();
+});
+
+app.get("/titi", (req, res, next) => {
+  res.end("hello world");
+});
+
+app.listen(3000, () => {
+  console.log("server started on port 3000");
+});
