2a340ef62ec6cd69425e8087faa66b0e65eb0c17
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-10-19 15:44:36 +0200

    angular hello world

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'front';
+  name = 'Jean-Louis';
+
+  constructor() {
+    setTimeout(() => {
+      this.name = 'JLG';
+    }, 2000);
+  }
 }
