a6ffce8a15dc3e0e23d6c9b0f3cbf07e37faaa52
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-10-20 17:10:16 +0200

    added create route

--- front/src/app/stock/stock-routing.module.ts
+++ front/src/app/stock/stock-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateComponent } from './create/create.component';
 
 import { StockComponent } from './stock.component';
 
-const routes: Routes = [{ path: '', component: StockComponent }];
+const routes: Routes = [
+  { path: '', component: StockComponent },
+  { path: 'create', component: CreateComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class StockRoutingModule { }
+export class StockRoutingModule {}
