8dc5ea6f91319fda6de4c2c62e0ba0f3fdddb625
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-10-21 15:05:32 +0200

    add localstorage

--- front/src/app/services/article.service.ts
+++ front/src/app/services/article.service.ts
@@ -5,18 +5,31 @@ import { Article } from '../interfaces/article';
   providedIn: 'root',
 })
 export class ArticleService {
-  articles: Article[] = [
-    { name: 'Tournevis', price: 1.2, qty: 230 },
-    { name: 'Tournevis cruciforme', price: 11.2, qty: 100 },
-    { name: 'Pelle', price: 3.4, qty: 230 },
-    { name: 'Pince', price: 5, qty: 230 },
-    { name: 'Couteau', price: 40, qty: 100 },
-    { name: 'Tondeuse à gazon', price: 120, qty: 20 },
-  ];
+  articles: Article[] = this.getArticles();
 
   constructor() {}
 
+  getArticles(): Article[] {
+    const str = localStorage.getItem('articles');
+    if (!str) {
+      return [
+        { name: 'Tournevis', price: 1.2, qty: 230 },
+        { name: 'Tournevis cruciforme', price: 11.2, qty: 100 },
+        { name: 'Pelle', price: 3.4, qty: 230 },
+        { name: 'Pince', price: 5, qty: 230 },
+        { name: 'Couteau', price: 40, qty: 100 },
+        { name: 'Tondeuse à gazon', price: 120, qty: 20 },
+      ];
+    }
+    return JSON.parse(str) as Article[];
+  }
+
+  save(): void {
+    localStorage.setItem('articles', JSON.stringify(this.articles));
+  }
+
   add(a: Article): void {
     this.articles.push(a);
+    this.save();
   }
 }
