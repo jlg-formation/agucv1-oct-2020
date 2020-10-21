19cf76ff8adcfd83235a1dbe4c0f7f0fa5f87959
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-10-21 15:39:24 +0200

    added selectable articles

--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from '../interfaces/article';
 import { ArticleService } from '../services/article.service';
 
 @Component({
@@ -7,7 +8,17 @@ import { ArticleService } from '../services/article.service';
   styleUrls: ['./stock.component.scss'],
 })
 export class StockComponent implements OnInit {
+  selectedArticles: Article[] = [];
   constructor(public articleService: ArticleService) {}
 
   ngOnInit(): void {}
+
+  toggle(a: Article): void {
+    console.log('toggle', a);
+    if (this.selectedArticles.includes(a)) {
+      this.selectedArticles = this.selectedArticles.filter((art) => art !== a);
+      return;
+    }
+    this.selectedArticles.push(a);
+  }
 }
