787ad678d59a573e6d512dcb305e4ca9cd3a093e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-10-21 12:03:04 +0200

    added ellipsis pipe

--- front/src/app/stock/stock.module.ts
+++ front/src/app/stock/stock.module.ts
@@ -4,13 +4,10 @@ import { CommonModule } from '@angular/common';
 import { StockRoutingModule } from './stock-routing.module';
 import { StockComponent } from './stock.component';
 import { CreateComponent } from './create/create.component';
-
+import { WidgetModule } from '../widget/widget.module';
 
 @NgModule({
   declarations: [StockComponent, CreateComponent],
-  imports: [
-    CommonModule,
-    StockRoutingModule
-  ]
+  imports: [CommonModule, StockRoutingModule, WidgetModule],
 })
-export class StockModule { }
+export class StockModule {}
---
+++ front/src/app/widget/ellipsis.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'ellipsis',
+})
+export class EllipsisPipe implements PipeTransform {
+  transform(value: string, maxLength = 10): string {
+    if (value.length > maxLength) {
+      return value.substr(0, maxLength) + '...';
+    }
+    return value;
+  }
+}
