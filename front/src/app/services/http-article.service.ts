00f2cfef56a1d78b753109c0433f6f99adf9286f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-10-22 09:48:35 +0200

    added back-end

--- front/src/app/services/http-article.service.ts
+++ front/src/app/services/http-article.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ArticleService } from './article.service';
+import { Article } from '../interfaces/article';
+
+const url = 'http://localhost:3000/ws/articles';
 
 @Injectable({
   providedIn: 'root',
@@ -13,6 +16,37 @@ export class HttpArticleService extends ArticleService {
   }
 
   load(): void {
-    this.http.get('http://localhost:3000/ws/articles').subscribe();
+    this.http.get<Article[]>(url).subscribe({
+      next: (articles) => {
+        console.log('articles: ', articles);
+        this.articles = articles;
+      },
+      error: (err) => {
+        console.log('err: ', err);
+      },
+      complete: () => {
+        console.log('complete');
+      },
+    });
+  }
+
+  add(a: Article): void {
+    super.add(a);
+    this.http.post<void>(url, a).subscribe({
+      next: () => {
+        console.log('next');
+        this.load();
+      },
+      error: (err) => {
+        console.log('err: ', err);
+      },
+      complete: () => {
+        console.log('complete');
+      },
+    });
+  }
+
+  refresh(): void {
+    this.load();
   }
 }
