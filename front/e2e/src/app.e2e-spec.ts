de34bc8bcae3777c6b495816286363d719bc5398
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-10-22 15:38:57 +0200

    updated e2e

--- front/e2e/src/app.e2e-spec.ts
+++ front/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Hello JLG');
+    expect(page.getTitleText()).toEqual('Gérer efficacement votre stock');
   });
 
   afterEach(async () => {
