1e94535dc9a443a5eacd7b98579eec6adb69c40a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-10-22 14:36:55 +0200

    coverage service ok

--- front/src/app/services/article.service.spec.ts
+++ front/src/app/services/article.service.spec.ts
@@ -14,4 +14,15 @@ describe('ArticleService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should remove at least with one article', () => {
+    service.articles = [{ id: 'asdfaf', name: 'asdfafds', price: 1, qty: 3 }];
+    service.remove([]);
+    expect(service).toBeTruthy();
+  });
+
+  it('should refresh', () => {
+    service.refresh();
+    expect(service).toBeTruthy();
+  });
 });
