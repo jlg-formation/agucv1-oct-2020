52e0ad4a413ce125c0afedc169768779c95a5b64
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-10-22 11:54:12 +0200

    added http testing controler

--- front/src/app/services/http-article.service.spec.ts
+++ front/src/app/services/http-article.service.spec.ts
@@ -1,19 +1,31 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 import { HttpArticleService } from './http-article.service';
 
 describe('HttpArticleService', () => {
   let service: HttpArticleService;
+  let http: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
     });
     service = TestBed.inject(HttpArticleService);
+    http = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    http.verify();
   });
 
   it('should be created', () => {
+    const req = http.expectOne('/ws/articles');
+    expect(req.request.method).toEqual('GET');
+    req.flush([]);
     expect(service).toBeTruthy();
   });
 });
